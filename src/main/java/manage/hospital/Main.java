957b358bcf2118eff83617054ba32769a7911f6b
sixthson6 <sudais2408@gmail.com>
2025-05-09 12:24:38 -0700

    Add test code for accessing elements in database

--- src/main/java/manage/hospital/Main.java
+++ src/main/java/manage/hospital/Main.java
@@ -1,21 +1,33 @@
 package manage.hospital;
 
-import java.sql.Connection;
-
-import manage.hospital.dao.DBConnection;
-import manage.hospital.util.HospitalDataSeeder;
+import manage.hospital.dao.PatientDAO;
+import manage.hospital.model.Patient;
 
 public class Main {
     public static void main(String[] args) {
-        try (Connection conn = DBConnection.getConnection()) {
-            // Run schema SQL script from resources/sql/hospital_schema.sql
-            // SQLExecutor.runSQLScript(conn, "/sql/hospital_schema.sql");
+        PatientDAO patient = new PatientDAO();
 
-            // SQLExecutor.runSQLScript(conn, "/sql/populate_schema.sql");
-            HospitalDataSeeder.seedData();
+        // HospitalDataSeeder.seedData(); // Seed the database with initial data
 
-        } catch (Exception e) {
-            System.err.println("❌ Failed to initialize or execute: " + e.getMessage());
-        }
+        Patient p1 = new Patient(10113, "Foreman Joes", "Eric", "12 Clinic Way", "555-2001");
+        Patient p2 = new Patient(11121, "Cameron Hall", "Allison", "98 Med St", "555-2002");
+        Patient p3 = new Patient(121145, "Park Jun", "Chi", "77 Hospital Lane", "555-2003");
+
+        patient.insertPatient(p1);
+        patient.insertPatient(p2);
+        patient.insertPatient(p3);
+
+        Patient updated = new Patient(101133, "Cameron", "Allison", "Updated Address", "555-9999");
+        patient.updatePatient(updated);
+
+        Patient p = patient.getPatient(101133);
+        System.out.println(p);
+
+        patient.deletePatient(101133);
+        
+        System.out.println("Reading: " + patient.getAllPatients());
+
+
+
     }
 }
